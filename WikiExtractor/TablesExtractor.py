3ea89e4299142ebbc16710231b823d91982e77b7
DialloBarry <safietoudiallo9@gmail.com>
2020-10-18 16:05:26 +0200

    intialization of the wikiextractor project

---
+++ WikiExtractor/TablesExtractor.py
@@ -0,0 +1,12 @@
+""" """
+class TablesExtractor:
+    """ An wikipedia table extractor"""
+    """
+    - Avoir un attribut pour l'url
+    - Avoir une methode pour requeter l'url
+    - Une methode isReleventTable(table)
+    - methode is_url_ok(url)
+    - methode get_all_tables
+    - methode pour ecrire une table dans un csv write_table_to_csv()
+    
+    """
---
+++ WikiExtractor/WikiExtractorMain.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+- une methode pour lire le fichier contenant les urls et stocke
+ces derniers dans un tableau
+- une fonction main() qui contient une boucle parcourant le tableau des urls précédent
+et qui utilise la classe TablesExtractor pour faire les traitements
+"""
