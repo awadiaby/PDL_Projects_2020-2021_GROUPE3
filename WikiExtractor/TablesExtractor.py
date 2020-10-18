4c87e31f811b061ee2df596d0dca2ce36174a193
DialloBarry <safietoudiallo9@gmail.com>
2020-10-18 21:09:52 +0200

    Creation of the class TablesExtractor and a main one. This version of the code can read, parse html wikipedia pages and write each table in a csv file

--- WikiExtractor/TablesExtractor.py
+++ WikiExtractor/TablesExtractor.py
@@ -1,5 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# HTML parsing
+import os
+from bs4 import BeautifulSoup as soup
+# Web client
+from urllib.request import urlopen as uReq
+import urllib.request
 """ """
 class TablesExtractor:
+
+    
     """ An wikipedia table extractor"""
     """
     - Avoir un attribut pour l'url
@@ -8,5 +18,58 @@ class TablesExtractor:
     - methode is_url_ok(url)
     - methode get_all_tables
     - methode pour ecrire une table dans un csv write_table_to_csv()
-    
     """
+    #constructor of class
+    #def __init__(self):
+    #   print("init")
+       
+    def is_ok_url(self, url):
+        request = urllib.request.Request(url)
+        request.get_method = lambda: 'HEAD'
+        try:
+            urllib.request.urlopen(request)
+            return True
+        except  urllib.request.HTTPError:
+            return False
+        
+    """Function extracting all tables from a given webpage, parameter = URL string"""
+    def get_all_tables(self, url):
+       if self.is_ok_url(url):
+            uClient = uReq(url)
+            page_html = uClient.read()
+            uClient.close()
+            page_soup = soup(page_html, "html.parser")
+            tables = page_soup.find_all("table")
+            print(tables[0])
+            return tables 
+    """
+    table : 
+    outputfolder : 
+    """
+    def write_table_to_csv(self, table, output_fileName, output_folder, delimiter=";"):
+       if not os.path.exists(output_folder):
+            os.mkdir(output_folder)
+       with open(output_folder+"/"+output_fileName, "w", encoding="utf-8") as output_file:
+        # Table header from <th> cells
+        header = ""
+        th_set = table.find_all("th")
+        for i in range(len(th_set)):
+            th_data = th_set[i].text.rstrip('\n')
+            if i == len(th_set) - 1:
+                header += f"{th_data}\n"
+            else:
+                header += f"{th_data}{delimiter}"
+        output_file.write(header)
+        # Table data from <td> cells for each <tr> row
+        tr_set = table.find_all("tr")
+        for tr in tr_set:
+            line = ""
+            td_set = tr.find_all("td")
+            for i in range(len(td_set)):
+                td_data = td_set[i].text.rstrip('\n')
+                if i == len(td_set) - 1:
+                    line += f"{td_data}\n"
+                else:
+                    line += f"{td_data}{delimiter}"
+            output_file.write(line)
+            
--- WikiExtractor/WikiExtractorMain.py
+++ WikiExtractor/WikiExtractorMain.py
@@ -5,3 +5,43 @@ ces derniers dans un tableau
 - une fonction main() qui contient une boucle parcourant le tableau des urls précédent
 et qui utilise la classe TablesExtractor pour faire les traitements
 """
+from TablesExtractor import TablesExtractor
+import os
+
+#output_folder=os.mkdir('C:\\Users\\safie\\Desktop\\PDL_WikiExtractor\\WikiExtractor\\output_folder')
+WIKI_BASE_URL = "https://en.wikipedia.org/wiki/"
+urls = {}
+
+def read_urls_file():
+    urlFilePath = './input_data/wikiUrls.txt'
+    if not os.path.exists(urlFilePath):
+        print("The urls file does'nt exists !!!")
+        exit(-1)
+    urlsFile = open(urlFilePath, "r")
+    for shortUrl in urlsFile.readlines():
+        urls[shortUrl] = WIKI_BASE_URL + shortUrl
+        #urls.append(WIKI_BASE_URL + shortUrl)
+    urlsFile.close()
+        
+
+
+
+
+
+#======================== Main ====================
+
+tablesExtractor = TablesExtractor()
+read_urls_file()
+output_folder = "output_folder"
+
+print("***************** Begin Extract *****************")
+for shortUrl, url in urls.items():
+    print("url : ", url)
+    tables = tablesExtractor.get_all_tables(url)
+    #tableIdex = 0
+    for tableIdex in range(len(tables)):
+        output_fileName = shortUrl.rstrip("\n") + '-' + str(tableIdex) + '.csv'
+        print("outputFileName : ",output_fileName)
+        tablesExtractor.write_table_to_csv(tables[tableIdex], output_fileName, output_folder)
+        
+print("***************** Extract finished *****************")
